5588efba99b085d12e7c1ac8b8542d67f7bae659
Dennis Henle <dennis.henle@sevdesk.de>
2022-10-28 08:14:31 +0200

    added gitignore

--- strategy_pattern/Strategy_Pattern/Duck.swift
+++ strategy_pattern/Strategy_Pattern/Duck.swift
@@ -7,34 +7,35 @@
 
 // Andere Klassen werden von Duck erben und sich selbst Flug- bzw. Quackverhalten geben.
 class Duck {
+    
     private var flyBehavior: FlyBehavior  // Flugstrategie wird in extra Klasse ausgelagert
     private var quackBehavior: QuackBehavior // Quackstrategie wird in extra Klasse ausgelagert
-    
+
     init(flyBehavior: FlyBehavior, quackBehavior: QuackBehavior) {
         self.flyBehavior = flyBehavior
         self.quackBehavior = quackBehavior
     }
-    
+
     func performFly() {
         flyBehavior.fly()
     }
-    
+
     func performQuack() {
         quackBehavior.quack()
     }
-    
+
     func setFlyBehavior(_ behavior: FlyBehavior) {
         flyBehavior = behavior
     }
-    
+
     func setQuackBehavior(_ behavior: QuackBehavior) {
         quackBehavior = behavior
     }
-    
+
     func walk() {
         print("i'm walking")
     }
-    
+
     func swim() {
         print("i'm swimming")
     }
