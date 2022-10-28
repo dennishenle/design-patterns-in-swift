809ff94ee3443c1719bcf7e7190bdd6ecce0ce04
Dennis Henle <dennis.henle@sevdesk.de>
2022-10-28 09:53:27 +0200

    created decorator pattern

---
+++ decorator-pattern/decorator-pattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  decorator-pattern
+//
+//  Created by Dennis Henle on 28.10.22.
+//  Copyright © 2022 sevDesk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
